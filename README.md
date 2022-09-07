c404bb80f5dbb005a1e4a2cdd8ce9784589e2e68
Adam Donnison <ajdonnison@gmail.com>
2022-09-07 14:46:40 +1000

    First cut of security scanning utilities/procedures

---
+++ README.md
@@ -0,0 +1,45 @@
+# Security scanning guide
+
+## Pentest procedure
+
+### Collect IP addresses of interest
+
+The `collect-ips.sh` script interrogates AWS Route53 and records one ip address per host
+for the purpose of scanning.  While multiple ips may exist they are usually load balancers
+and don't affect the scanning, apart from slowing it down.
+
+Output is two files, `dns_hosts` and `dns_ips`.  The first is a host/ip pair list,
+which may include duplicates due to different hostnames for the same instance, and
+non-routable hosts.
+
+the second just the unique remotely addressable ip addresses.
+
+Note that this script requires AWS authentication and may need to be run
+separately and supplied to the scanning service
+
+### Discovery scanning
+
+Run nmap via metasploit to do initial scanning
+
+```
+msfconsole -o /reports/gather_report -x "db_nmap -A -oA /reports/nmap_scan -iL /root/dns_ips; exit"
+```
+
+This both populates the metasploit database and creates a scan report in
+multiple formats.  The XML format is useful for automation.
+
+### Common ports
+
+Check common ports in metasploit database:
+
+mfsconsole -o port_report -x "services; exit"
+
+### Check for interesting services
+
+Wordpress
+OpenVPN
+RDS (Postgres)
+
+### Check API limits
+
+Tool: locust, scripts in `locust/`
---
+++ scripts/collect-ips.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Collects IP addresses of interest from running instances, load balancers and DNS
+# Requires a domain name to interrogate
+if (( $# == 0 )); then
+  echo "You must supply a domain name"
+fi
+
+domain="$1"
+
+# First running instances
+#aws ec2 describe-instances --query 'Reservations[].Instances[].PublicIpAddress' \
+#  --output text | tr -s '\t' '\n' > iplist
+
+# Load balancers
+#aws elbv2 describe-load-balancers --query 'LoadBalancers[].DNSName' --output text \
+#  | xargs -n 1 host | sed -n '/has address/s/^.*address\s//p' >> iplist
+
+# DNS, note we don't want records that point to assets we don't control
+aws route53 list-resource-record-sets --hosted-zone-id $(aws route53 list-hosted-zones \
+  --query "HostedZones[?Name == '${domain%.}.'].Id" --output text) \
+  --query "ResourceRecordSets[?(Type == 'A' || Type == 'CNAME')].Name" \
+  --output text|tr -s '\t' '\n' |grep -Ev '(^_|mail|domainkey|dkim|em[0-9]|link|mail|status)' \
+  > hostnames
+
+# For each hostname get one IP address if possible
+for host in $(< hostnames); do
+  addr=$(host ${host} | grep 'has address' | head -1)
+  if [[ -n ${addr} ]]; then
+     read dns has address ip <<!
+${addr}
+!
+     echo "${host%.} ${ip}"
+  fi
+done > dns_hosts
+
+# Now make sure we only have a single IP address for each
+cut -d\  -f2 dns_hosts | grep -Ev '^(10\.|172\.3[12])' | sort -u > dns_ips
+
+# There will be double ups from DNS and previous resources, so we do a unique sort
+# and also remove any non-routable (private CIDR) ip addresses
+#sort -u iplist | egrep -v '^(10|172\.3[12])\.' > ips_of_interest

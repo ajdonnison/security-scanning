fff0e3d9c1471e7915af05f84d7a7ce1074476ef
Adam Donnison <ajdonnison@gmail.com>
2022-10-28 15:26:01 +1100

    Replace lambda with step function

--- scripts/replace-hosts.py
+++ scripts/replace-hosts.py
@@ -6,7 +6,7 @@ import os.path
 def replace():
     parser = ArgumentParser()
     parser.add_argument('--data-dir', '-d', help='data directory', default='data')
-    parser.add_argument('--hosts-file', '-h', help='hosts-file', default='dns_hosts')
+    parser.add_argument('--hosts-file', '-f', help='hosts-file', default='dns_hosts')
     parser.add_argument('--report-dir', '-r', help='report directory', default='reports')
     parser.add_argument('--report-name', '-n', help='report name', required=True)
     args = parser.parse_args()
@@ -21,9 +21,12 @@ def replace():
     # Now parse the input file
     with open(os.path.join(args.report_dir,args.report_name)) as report:
         for line in report:
-            if line.startswith('Nmap scan report for'):
-                TODO:  replace pattern here.
-            else:
+            found = False
+            for key in xref:
+                if key in line:
+                   found = True
+                   print(line.rstrip().replace(key, xref[key]))
+            if not found:
                 print(line.rstrip())
        
 

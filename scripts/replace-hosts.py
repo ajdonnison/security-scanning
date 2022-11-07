c882e282c7c0de10d09366036d420482b186ac32
Adam Donnison <ajdonnison@gmail.com>
2022-11-07 14:14:39 +1100

    Make sure we update reports with DNS names

--- scripts/replace-hosts.py
+++ scripts/replace-hosts.py
@@ -2,13 +2,14 @@
 
 from argparse import ArgumentParser
 import os.path
+import sys
 
 def replace():
     parser = ArgumentParser()
     parser.add_argument('--data-dir', '-d', help='data directory', default='data')
     parser.add_argument('--hosts-file', '-f', help='hosts-file', default='dns_hosts')
     parser.add_argument('--report-dir', '-r', help='report directory', default='reports')
-    parser.add_argument('--report-name', '-n', help='report name', required=True)
+    parser.add_argument('--report-name', '-n', help='report name', default='-')
     args = parser.parse_args()
 
     # Load our cross reference
@@ -19,15 +20,19 @@ def replace():
             xref[ip] = host
 
     # Now parse the input file
-    with open(os.path.join(args.report_dir,args.report_name)) as report:
-        for line in report:
-            found = False
-            for key in xref:
-                if key in line:
-                   found = True
-                   print(line.rstrip().replace(key, xref[key]))
-            if not found:
-                print(line.rstrip())
+    if args.report_name == "-":
+        report = sys.stdin
+    else:
+        report = open(os.path.join(args.report_dir,args.report_name))
+
+    for line in report:
+        found = False
+        for key in xref:
+            if key in line:
+               found = True
+               print(line.rstrip().replace(key, xref[key]))
+        if not found:
+            print(line.rstrip())
        
 
 if __name__ == '__main__':
